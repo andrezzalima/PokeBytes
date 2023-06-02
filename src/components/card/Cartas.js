d62d350dcfcdc09bf9c1cd69ab1e2c0fa864a4a2
unknown <hugojoaoraposo@gmail.com>
2023-06-02 10:32:00 +0100

    first commit

---
+++ src/components/card/Cartas.js
@@ -0,0 +1,20 @@
+import './Cartas.css'
+
+
+function Cartas(){
+    return(
+        <div>
+            <div className="cards">
+                <h3>Pikachu</h3>
+                <div className='image'>
+        
+                </div>
+                <h5>Tail Whap</h5>
+                <p>Pikachu is a short, chubby rodent Pokémon. It is covered in yellow fur.</p>
+                <div className='powers'>Ataque: </div>
+            </div>
+        </div>
+    )
+}
+
+export default Cartas
