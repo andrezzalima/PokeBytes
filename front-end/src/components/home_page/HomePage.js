7448890bae1512180f6b87c1ff6674d100883314
unknown <hugojoaoraposo@gmail.com>
2023-06-05 13:14:00 +0100

    started the Trade Machine page and css

--- front-end/src/components/home_page/HomePage.js
+++ front-end/src/components/home_page/HomePage.js
@@ -1,4 +1,4 @@
-import "./HomePage.css"; 
+import "./HomePage.css";
 import "./Icones/tooltip.css";
 import React from "react";
 import { Link } from 'react-router-dom'
@@ -19,46 +19,46 @@ function HomePage() {
         <Dropdown />
       </div>
 
-      
-      <div className="iconBattle">
-  
-  <div className="tooltip imgBattle">
-    <ButtonIcon image={battle} tooltip="PackOpening" alt="Packs Icon" />
-    <span className="tooltiptext">Let's Battle!</span> 
-  </div>
 
-        
-        
+      <div className="iconBattle">
+
+        <div className="tooltip imgBattle">
+          <ButtonIcon image={battle} tooltip="PackOpening" alt="Packs Icon" />
+          <span className="tooltiptext">Let's Battle!</span>
+        </div>
+
+
+
       </div>
 
 
 
-        <div className="iconsMeio">
-          <div className="tooltip"> 
-            <ButtonIcon image={packs} tooltip="PackOpening" alt="Packs Icon" />
-            <span className="tooltiptext">Pack Opening</span> 
-          </div>
-
-          <div className="tooltip"> 
-            <ButtonIcon image={pokecoins} tooltip="PokeCoins" alt="Packs Icon" />
-            <span className="tooltiptext">PokéCoins</span> 
-          </div>
-
+      <div className="iconsMeio">
+        <div className="tooltip">
+          <ButtonIcon image={packs} tooltip="PackOpening" alt="Packs Icon" />
+          <span className="tooltiptext">Pack Opening</span>
         </div>
 
-        <div className="divCima">
-          <div className="tooltip"> 
+        <div className="tooltip">
+          <ButtonIcon image={pokecoins} tooltip="PokeCoins" alt="Packs Icon" />
+          <span className="tooltiptext">PokéCoins</span>
+        </div>
+
+      </div>
+
+      <div className="divCima">
+        <div className="tooltip">
           <Link to="/homePage/tradeMachine"><ButtonIcon image={trademachine} tooltip="TradeMachine" alt="Packs Icon" /></Link>
-            <span className="tooltiptext">Trading Machine</span> 
-          </div>
-        
+          <span className="tooltiptext">Trading Machine</span>
+        </div>
 
-        <div className="tooltip"> 
+
+        <div className="tooltip">
           <Link to="/homePage/pokeBag"><ButtonIcon image={pokebag} tooltip="PokeBag" alt="Packs Icon" /></Link>
-          <span className="tooltiptext icon">PokéBag</span> 
+          <span className="tooltiptext icon">PokéBag</span>
         </div>
-        </div>
-      
+      </div>
+
     </div>
   );
 }
--- front-end/src/components/home_page/tradeMachine/TradeMachine.js
+++ front-end/src/components/home_page/tradeMachine/TradeMachine.js
@@ -1,11 +1,36 @@
+import React, { useState } from 'react';
+import ButtonIcon from '../Icones/ButtonIcon';
+import './TradeMachine.css';
 
+function TradeMachine(props) {
+  const [showRules, setShowRules] = useState(false); // Estado que verifica se as regras devem ser mostradas ou escondidas
 
-function TradeMachine(){
-    return (
-        <div>
-            Olá da TradeMachine
+  const toggleRules = () => {
+    setShowRules(!showRules); // Muda o estado showRules quando a div "rules" é clicada
+  };
+
+  return (
+    <div>
+      <div className="background-tradeMachine">
+        <div className="rules" onClick={toggleRules}> 
+        <span className='rules-text'>RULES</span>
+          
+          {showRules && (
+            <div className="rules-content-box">
+              <p className="rules-content">
+                It's time to trade Pokémon cards with the computer, trainer! Prepare for some exciting swaps!
+                Remember, you can only trade cards with the computer that share the same rarity. Rarity is like a Pokémon's uniqueness level!
+                The available rarities for trading are: very common, common, uncommon, and rare. Let's keep it fair and fun!
+                Sorry, legendary trainers! Pokémon cards beyond rare (like very rare, epic, and legendary) are off-limits for trading. They're just too precious!
+                Your trusty program buddy will help you find the perfect trading partner based on the rarity you desire. Let the trading adventures begin!
+                These rules aim to capture the spirit of Pokémon, creating an engaging and playful atmosphere for card trading while incorporating the concept of rarity in a lighthearted way. Happy trading, Pokémon trainers!
+              </p>
+            </div>
+          )}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default TradeMachine
+export default TradeMachine;
