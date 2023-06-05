5ad513d68ad8de7bf4bbf63229cdf72b9dd7c13d
unknown <hugojoaoraposo@gmail.com>
2023-06-05 18:20:05 +0100

    instalei o Tailwind, para conseguirem ver as cenas devem ter que fazer npm i outra vez segundo o Rafa

--- front-end/src/components/home_page/tradeMachine/TradeMachine.js
+++ front-end/src/components/home_page/tradeMachine/TradeMachine.js
@@ -1,32 +1,48 @@
 import React, { useState } from 'react';
-import ButtonIcon from '../Icones/ButtonIcon';
+import { Link } from 'react-router-dom';
 import './TradeMachine.css';
+import returnIcon from "../../../icons/return_icon1.png";
+import pokeballBackground from "../../../icons/pokeball_background.png";
 
 function TradeMachine(props) {
-  const [showRules, setShowRules] = useState(false); // Estado que verifica se as regras devem ser mostradas ou escondidas
+  const [showRules, setShowRules] = useState(false);
+
 
   const toggleRules = () => {
-    setShowRules(!showRules); // Muda o estado showRules quando a div "rules" é clicada
+    setShowRules(!showRules);
   };
+
+
+
+
 
   return (
     <div>
       <div className="background-tradeMachine">
-        <div className="rules" onClick={toggleRules}> 
-        <span className='rules-text'>RULES</span>
-          
-          {showRules && (
-            <div className="rules-content-box">
-              <p className="rules-content">
-                It's time to trade Pokémon cards with the computer, trainer! Prepare for some exciting swaps!
-                Remember, you can only trade cards with the computer that share the same rarity. Rarity is like a Pokémon's uniqueness level!
-                The available rarities for trading are: very common, common, uncommon, and rare. Let's keep it fair and fun!
-                Sorry, legendary trainers! Pokémon cards beyond rare (like very rare, epic, and legendary) are off-limits for trading. They're just too precious!
-                Your trusty program buddy will help you find the perfect trading partner based on the rarity you desire. Let the trading adventures begin!
-                These rules aim to capture the spirit of Pokémon, creating an engaging and playful atmosphere for card trading while incorporating the concept of rarity in a lighthearted way. Happy trading, Pokémon trainers!
+        <div className="rules" onClick={toggleRules}>
+          <span className='rules-text' >RULES</span>
+        </div>
+        {showRules && (
+          <div style={{border: "10px solid #192653"}} className='rules-content-box bg-white'>
+            <img src={pokeballBackground} className='pokeball-background' alt="Pokeball Background" />
+            <div className="rules-content">
+              <p><span className='rules-title'>It's time to trade Pokémon cards, trainer! Prepare for some exciting swaps!</span> <br></br><br></br>
+                Remember, you can only trade cards that share the same rarity type, which is like a Pokémon's uniqueness level! <br></br><br></br>
+                The available rarities for trading are: <span className='very-common'>very common</span>, <span className='common'>common</span>, <span className='uncommon'>uncommon</span>, and <span className='rare'>rare</span>. Let's keep it fair and fun! <br></br><br></br>
+                Sorry, legendary trainers! Pokémon cards beyond rare (like <span className='very-rare'>very rare</span>, <span className='epic'>epic</span>, and <span className='legendary'>legendary</span>) are off-limits for trading. They're just too precious! <br></br><br></br>
+                Your trusty program buddy will help you find the perfect trading partner based on the rarity you desire. Let the trading adventures begin! <br></br><br></br>
+                These rules aim to capture the spirit of Pokémon, creating an engaging and playful atmosphere. <br></br><br></br>
+                Happy trading, Pokémon trainers!
               </p>
+              
             </div>
-          )}
+          </div>
+        )}
+
+        <div className='return-wrapper'>
+          <div className='return-to-homepage' >
+            <Link to = "/homePage"> <img src={returnIcon} className='return-icon' alt="Return to Homepage" /> </Link> 
+          </div>
         </div>
       </div>
     </div>
