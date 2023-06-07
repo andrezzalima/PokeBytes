d341d15d0959df0f192f20d6c2b6b091587a4d3c
unknown <hugojoaoraposo@gmail.com>
2023-06-07 12:43:35 +0100

    adientei a carta final, fiz os backgrounds para todos os tipos de carta

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'verycommon': '#804320',
+        'common': '#32D61F',
+        'uncommon': '#008B8B',
+        'rare': '#309BF0',
+        'veryrare': '#E12C6A',
+        'epic': '#FF00FF',
+        'legenday': '#FFA500'
+        
+      }
+    },
   },
   plugins: [],
 }
