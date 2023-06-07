4f0a32239a7b0524c55900360976e0d89dc58cc0
Ruben <ruben.teixeiraleao2001@gmail.com>
2023-06-07 17:43:11 +0100

    att Ruben

--- front-end/src/components/home_page/PokeCoins/PaymentPage.js
+++ front-end/src/components/home_page/PokeCoins/PaymentPage.js
@@ -1,62 +1,103 @@
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import "./PaymentPage.css"
+import "./PaymentPage.css";
 
 function PaymentPage() {
+  const [formData, setFormData] = useState({
+    firstName: "Ruben",
+    lastName: "",
+  });
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    localStorage.setItem("paymentInfo", JSON.stringify(formData));
+  };
+
+  useEffect(() => {
+    const storedPaymentInfo = localStorage.getItem("paymentInfo");
+    if (storedPaymentInfo) {
+      const parsedPaymentInfo = JSON.parse(storedPaymentInfo);
+      setFormData(parsedPaymentInfo);
+    }
+  }, []);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
   return (
     <div className="background-payment">
-      <div class="wrapper">
-        <div class="container">
-          <form action="">
+      <div className="wrapper">
+        <div className="container">
+          <form onSubmit={handleFormSubmit}>
             <h1 className="h1">
-              <i class="fas fa-shipping-fast"></i>
+              <i className="fas fa-shipping-fast"></i>
               Payment
             </h1>
-            <div class="name">
+            <div className="name">
               <div>
-                <label for="f-name">First</label>
-                <input type="text" name="f-name" />
+                <label htmlFor="f-name">First</label>
+                <input
+                  type="text"
+                  name="firstName"
+                  value={formData.firstName}
+                  onChange={handleInputChange}
+                />
               </div>
               <div>
-                <label for="l-name">Last</label>
-                <input type="text" name="l-name" />
+                <label htmlFor="l-name">Last</label>
+                <input
+                  type="text"
+                  name="lastName"
+                  value={formData.lastName}
+                  onChange={handleInputChange}
+                />
               </div>
             </div>
-            <div class="street">
-              <label for="name">Street</label>
+            <div className="street">
+              <label htmlFor="name">Street</label>
               <input type="text" name="address" />
             </div>
-            <div class="address-info">
+            <div className="address-info">
               <div>
-                <label for="city">City</label>
+                <label htmlFor="city">City</label>
                 <input type="text" name="city" />
               </div>
               <div>
-                <label for="state">State</label>
+                <label htmlFor="state">State</label>
                 <input type="text" name="state" />
               </div>
               <div>
-                <label for="zip">Zip</label>
+                <label htmlFor="zip">Zip</label>
                 <input type="text" name="zip" />
               </div>
             </div>
 
-            <div class="cc-num">
-              <label for="card-num">Credit Card No.</label>
+            <div className="cc-num">
+              <label htmlFor="card-num">Credit Card No.</label>
               <input type="text" name="card-num" />
             </div>
-            <div class="cc-info">
+            <div className="cc-info">
               <div>
-                <label for="card-num">Exp</label>
+                <label htmlFor="card-num">Exp</label>
                 <input type="text" name="expire" />
               </div>
               <div>
-                <label for="card-num">CCV</label>
+                <label htmlFor="card-num">CCV</label>
                 <input type="text" name="security" />
               </div>
             </div>
-            <div class="btns">
-              <Link to="/homePage/pokecoins/payment/sucess"><button className="button-payment">Purchase</button></Link>
-              <Link to="/homePage/pokecoins/"><button className="button-payment">Cancel</button></Link>
+            <div className="btns">
+              <Link to="/homePage/pokecoins/payment/success">
+                <button className="button-payment">Purchase</button>
+              </Link>
+              <Link to="/homePage/pokecoins/">
+                <button className="button-payment">Cancel</button>
+              </Link>
             </div>
           </form>
         </div>
@@ -66,3 +107,4 @@ function PaymentPage() {
 }
 
 export default PaymentPage;
+
